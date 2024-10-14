942bb4cd26f06184abe03419b6698e81fdd250e0
goodsoundisme <sol000619@naver.com>
2024-10-14 22:18:25 +0900

    [init]cardservice에서 사용자 인증이 필요해 리포지토리 생성

---
+++ src/main/java/com/practice/queenstrello/domain/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.practice.queenstrello.domain.user.repository;
+
+import com.practice.queenstrello.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
