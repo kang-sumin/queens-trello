58062cf40d96a621e22ee4357ea0f5ac406f3f97
wlsql852 <wlsql852@naver.com>
2024-10-15 13:53:35 +0900

    [COMMENT] 통합검색 주석 추가

--- src/main/java/com/practice/queenstrello/domain/search/controller/SearchController.java
+++ src/main/java/com/practice/queenstrello/domain/search/controller/SearchController.java
@@ -13,6 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class SearchController {
     private final SearchService searchService;
 
+    /**
+     * 카드 통합검색
+     * @param page 페이지 번호
+     * @param size 각 페이지 당 카드 개수
+     * @param title 검색할 제목(포함)
+     * @param content 검색할 내용(포함)
+     * @param deadline 마감날짜 (ex-2024-10-24, 정확)
+     * @param nickname 담당자 닉네임(정확)
+     * @return 해당 검색 조건이 맞는 카드 목록(페이징)
+     */
     @GetMapping("/search")
     public Page<SearchResponse> searchCard (
                                     @RequestParam(required = false, defaultValue = "1") int page,
--- src/main/java/com/practice/queenstrello/domain/search/service/SearchService.java
+++ src/main/java/com/practice/queenstrello/domain/search/service/SearchService.java
@@ -18,10 +18,15 @@ public class SearchService {
     private final CardRepository cardRepository;
     private final UserRepository userRepository;
 
+    //카드 통랍검색
     public Page<SearchResponse> searchCard(int page, int size, String title, String content, String deadline, String nickname) {
+        //기본값으로 manager는 null
         User manager = null;
+        //닉네임 있으면 해당 닉네임을 갖는 manager를 찾기
         if (nickname !=null) manager = userRepository.findByNickname(nickname).orElseThrow(()-> new NoNicnameUserException(nickname));
+        //pageable만들기
         Pageable pageable = PageRequest.of(page-1, size);
+        //통합검색 -> responseDto형태로 변환후 내보내기
         return cardRepository.searchCard(title, content, deadline, manager, pageable)
                 .map(card -> new SearchResponse(card.getTitle(),card.getContent(),card.getDeadLine(),card.getCardManagers()));
     }
