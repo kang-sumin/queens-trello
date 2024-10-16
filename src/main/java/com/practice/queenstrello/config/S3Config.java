61a09a1708ddcbacfad4d154a48f914246e48396
nodaji <sniper.nhj@gmail.com>
2024-10-16 16:05:27 +0900

    [FEAT] S3Config 생성 / application.properties에 AWS S3 설정

---
+++ src/main/java/com/practice/queenstrello/config/S3Config.java
@@ -0,0 +1,29 @@
+package com.practice.queenstrello.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class S3Config {
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3Client() { // 반환 타입을 AmazonS3로 수정
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
